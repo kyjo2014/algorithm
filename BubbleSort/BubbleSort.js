639fc9e26b297e60b6050a3f69581a9f01251193
kyjo2014 <467346087@qq.com>
2017-03-07 22:55:39 +0800

    冒泡排序如果有序直接输出

--- BubbleSort/BubbleSort.js
+++ BubbleSort/BubbleSort.js
@@ -21,5 +21,31 @@ function bubbleSort(arr) {
 
 }
 
+/**
+ * 改进冒泡排序
+ * 当检测到一次遍历时候没有发生交换
+ * 说明数组已经有序
+ * @param {Array} arr 
+ */
+function bubbleSortv2(arr) {
+    let swapped = true
+    let len = arr.length
+    let temp = 0;
+    for (let i = 0; i < len - 1; i++) {
+        swapped = false //每次开始一次冒泡前都需要重置状态
+        for (let j = 0; j < len - 1 - j; j++) {
+            if (arr[j] < arr[j + 1]) {
+                temp = arr[j]
+                arr[j] = arr[j + 1]
+                arr[j + 1] = temp
+                swapped = true
+            }
+            if (!swapped) {
+                break
+            }
+        }
+        return arr
+    }
+}
 
 module.exports = bubbleSort
