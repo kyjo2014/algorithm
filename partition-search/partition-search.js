9d21183dae49fb766d6b4b5f0c441db9dbb37054
kyjo2014 <467346087@qq.com>
2017-03-23 00:08:32 +0800

    增加划分查找的解释

--- partition-search/partition-search.js
+++ partition-search/partition-search.js
@@ -1,3 +1,11 @@
+/**
+ * 增加划分查找，
+ * 利用快排的划分函数实现对数组的部分排序,利用中点位置确定的方法对整个数组进行划分
+ * 剑指offer29题目
+ * @param {any} numbers 
+ * @param {any} length 
+ * @returns 
+ */
 function MoreThanHalfNum(numbers, length) {
     var middle = length >> 1,
         start = 0,
