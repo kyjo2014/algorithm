2988ba530f57a1f23266ce522744a02b78d64af0
kyjo2014 <467346087@qq.com>
2017-04-05 23:22:26 +0800

    重建二叉树C++代码写完，数据结构没改成JS

--- 重建二叉树/index.js
+++ 重建二叉树/index.js
@@ -1,4 +1,4 @@
-function node(val,left,right) {
+function node(val, left, right) {
     this.val = val
     this.left = left || NULL
     this.right = right || NULL
@@ -10,7 +10,33 @@ function Construct(preorder, inorder, length) {
     if (preorder == NULL || inorder == NULL || length <= 0) {
         return NULL
     }
-    return ConstructCore(preorder,,inorder,)
+    return ConstructCore(preorder, , inorder, )
 }
 
-function ConstructCore(preorder,startPreorder, endPreorder,startInorder,endInorder)
+function ConstructCore(preorder, startPreorder, endPreorder, inorder, startInorder, endInorder) {
+    var rootValue = startPreorder[0]
+    root = new node(rootValue, NULL, NULL)
+    if (startPreorder == endPreorder) {
+        if (startInorder == endInorder) {
+            if (startInorder == endInorder && preorder[startPreorder] == preorder[startInorder]) {
+                return root
+            } else {
+                throw new Error('invalid input')
+            }
+        }
+        var rootInorder = startInorder
+        while (rootInorder <= endInorder && inorder != rootValue) {
+            ++rootInorder
+        }
+        if (rootInorder == endInorder && inorder[rootInorder] != rootValue) {
+            throw new Error('invalid input')
+        }
+        var leftLength = rootInorder - startInorder
+        var leftPreorder = startPreorder +leftLength
+        if (leftLength > 0) {
+            root.right =ConstructCore()
+        }
+        return root
+    }
+
+}
